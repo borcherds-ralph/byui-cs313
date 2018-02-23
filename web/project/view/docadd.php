59a45f7f7c2fdd41d086abe7c7a48f416d2a802d
Ralph Borcherds <ralph@mrbconsulting.biz>
2018-02-23 13:31:34 -0800

    week 7

--- web/project/view/docadd.php
+++ web/project/view/docadd.php
@@ -17,7 +17,7 @@
         <?php include 'common/header.php'; ?>
     
     <main>
-        <form action="<?php echo $basepath; ?>/?action=DocAdd" method="post" id="DocAdd" >
+        <form action="<?php echo $basepath; ?>/?action=DocAdd" method="post" id="editDoc" >
             <fieldset>
                 <div>
                     <input id="docfirstname" name="docfirstname" type="text" required tabindex="1"
