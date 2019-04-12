ad01ac1c0af068ed12818848162c9e4cbed66c11
Ralph Borcherds <ralph@mrbconsulting.biz>
2019-04-11 19:47:18 -0700

    Fix DB issue

--- web/project/config/database.php
+++ web/project/config/database.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 
 function get_db() {
 	$db = NULL;
 	try {
 		// default Heroku Postgres configuration URL
 		$dbUrl = getenv('DATABASE_URL');
-
+		if (!isset($dbUrl) || empty($dbUrl)) {
+		      $dbUrl = "postgres://ralphb:m1dk1h8n1@localhost:5433/doclookup";
+		    }
 		// Get the various parts of the DB Connection from the URL
 		$dbopts = parse_url($dbUrl);
 		$dbHost = $dbopts["host"];
@@ -13,7 +15,7 @@ function get_db() {
 		$dbUser = $dbopts["user"];
 		$dbPassword = $dbopts["pass"];
 		$dbName = ltrim($dbopts["path"],'/');
-		
+
 		// Create the PDO connection
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
